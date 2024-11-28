8b24ae4519fe15ddfdafa9d669208fe639563db1
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-11-28 09:49:47 -0300

    Added instructions to second test with differents faces

--- test_presence_list.py
+++ test_presence_list.py
@@ -11,3 +11,7 @@ def test_images_with_same_face(monkeypatch, capsys):
   assert difference < TOLERANCE
 def test_images_with_different_faces(monkeypatch, capsys):
   """ Test script with images of different persons"""
+  person1_image1 = './samples/person1_image0.jpg'
+  person1_image2 = './samples/person2_image0.jpg'
+  difference = compare_image_faces(person1_image1, person1_image2)
+  assert difference > TOLERANCE
