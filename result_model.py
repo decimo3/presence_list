89bf3bf5b9036edd20794eb5b15e6633d8fbc901
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-11-28 09:55:21 -0300

    Added model class to hold values of comparison

---
+++ result_model.py
@@ -0,0 +1,6 @@
+''' Class to hold values from face comparisson '''
+from typing import NamedTuple
+class Result(NamedTuple):
+  ''' Class to hold values from face comparisson '''
+  difference: float
+  is_match: bool
